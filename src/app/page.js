fedd2fbf1e0a2cbeecc27873bfcdbe1a4ba79682
Gaurav Raj <gauravraj6767@gmail.com>
2025-05-23 20:19:06 +0530

    Removed API call on home page

--- src/app/page.js
+++ src/app/page.js
@@ -15,26 +15,6 @@ export default function Home() {
   const router = useRouter()
   console.log("TEST ENDPOINT in -----   " + DJANGO_API_ENDPOINT)
 
-  const { data, error, isLoading } = useSWR(`${DJANGO_API_ENDPOINT}/hello`, fetcher);
-
-  if (error) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-red-100 text-red-700 border border-red-400 px-6 py-4 rounded-md shadow-md animate-fade-in">
-          <p className="font-semibold text-lg">Oops! API Server is Down.</p>
-          <p className="text-sm mt-1">Please check your connection or try again later.</p>
-        </div>
-      </div>
-    );
-  }
-
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
-      </div>
-    );
-  }
 
   return (
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-b from-blue-50 to-blue-100 dark:from-zinc-900 dark:to-zinc-800">
