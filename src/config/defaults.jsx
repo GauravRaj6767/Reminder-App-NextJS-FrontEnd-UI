5c0bb9b9eeef04f8fa97de7e58309641a0d760ee
Gaurav Raj <gauravraj6767@gmail.com>
2025-05-23 19:29:47 +0530

    Minor changes

--- src/config/defaults.jsx
+++ src/config/defaults.jsx
@@ -1,2 +1,2 @@
-export const DJANGO_API_BASE_URL = process.env.DJANGO_API_BASE_URL ? process.env.DJANGO_API_BASE_URL : `http://127.0.0.1:${DJANGO_PORT}`
+export const DJANGO_API_BASE_URL = process.env.DJANGO_API_BASE_URL
 export const DJANGO_API_ENDPOINT = `${DJANGO_API_BASE_URL}/api`
